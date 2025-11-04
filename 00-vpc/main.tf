edf62b1406a0179b191da1b2804361dce981cd98
brahmaiah <bramhaiahg@gmail.com>
2025-11-04 08:44:04 +0530

    commit it

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/daws-86s/terraform-aws-vpc.git?ref=main"
+    source = "git::https://github.com/srinivasateja73/terraform-aws-vpc.git?ref=main"
     # VPC
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
