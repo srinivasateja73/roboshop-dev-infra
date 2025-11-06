c4faafb1453265278ef2f047907de993021d924a
brahmaiah <bramhaiahg@gmail.com>
2025-11-06 12:07:15 +0530

    commit it

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "dawsteja-remote-bucket-for-terraform"
+    bucket = "dawsteja-remote-bucket-for-terraforms"
     key    = "baston-terraform-key"
     region = "us-east-1"
     use_lockfile = true
