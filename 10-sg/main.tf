fdf1adcde025154677fe2931e87ab24427cd947b
brahmaiah <bramhaiahg@gmail.com>
2025-11-07 07:29:35 +0530

    commit it

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -21,11 +21,11 @@ module "sg" {
 }
 
 # Frontend accepting traffic from frontend ALB
-resource "aws_security_group_rule" "frontend_frontend_alb" {
-  type              = "ingress"
-  security_group_id = module.sg[9].sg_id # frontend SG ID
-  source_security_group_id = module.sg[11].sg_id # frontend ALB SG ID
-  from_port         = 80
-  protocol          = "tcp"
-  to_port           = 80
-}
+# resource "aws_security_group_rule" "frontend_frontend_alb" {
+#   type              = "ingress"
+#   security_group_id = module.sg[9].sg_id # frontend SG ID
+#   source_security_group_id = module.sg[11].sg_id # frontend ALB SG ID
+#   from_port         = 80
+#   protocol          = "tcp"
+#   to_port           = 80
+# }
